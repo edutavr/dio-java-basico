2178d132b65f46ec6de2722088b32ca3d92b55fb
Eduardo <eduardotavorar@gmail.com>
2025-02-24 18:26:17 -0300

    spring data jpa

---
+++ aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -0,0 +1,27 @@
+package dio.aula;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import dio.aula.model.User;
+import dio.aula.repository.UserRepository;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("EDUARDO"); 
+        user.setUsername("edu.rocha");
+        user.setPassword("dio123");
+
+        repository.save(user);
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+    
+}
