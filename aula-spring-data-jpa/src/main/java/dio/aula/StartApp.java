3ed998dd76562bc5eb3b65f7beee2dfb614b7c39
Eduardo <eduardotavorar@gmail.com>
2025-02-25 21:08:36 -0300

    interação com o postgres

--- aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -1,5 +1,6 @@
 package dio.aula;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +9,7 @@ import dio.aula.repository.UserRepository;
 
 @Component
 public class StartApp implements CommandLineRunner {
-
+    @Autowired
     private UserRepository repository;
     @Override
     public void run(String... args) throws Exception {
